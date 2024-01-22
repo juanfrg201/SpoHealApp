e689ab66a3edac799157d8afc543046f3fc1be50
Juan Felipe <juanfrg2001@gmail.com>
2024-01-22 11:18:17 -0500

    Adds pwa configuration

---
+++ public/service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'SpoHealApp-Cache';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/manifest.json',
+        '/favicon.ico'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
