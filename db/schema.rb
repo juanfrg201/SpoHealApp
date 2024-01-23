e75fcc71c7ded0faa2d69b2bb24d9a0c2f01e1ca
jorgeRamirez07 <janramirezp@unbosque.edu.co>
2024-01-23 08:10:22 -0500

    Comunidades

--- db/schema.rb
+++ db/schema.rb
@@ -88,14 +88,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_23_000459) do
     t.index ["user_id"], name: "index_community_posts_on_user_id"
   end
 
-  create_table "sport_users", force: :cascade do |t|
-    t.string "name"
-    t.string "last_name"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "user_activities", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "activity_id", null: false
