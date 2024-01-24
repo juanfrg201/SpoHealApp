23a712f7b4de2b178de0b7286c22aa18dc36689e
Juan Felipe <juanfrg2001@gmail.com>
2024-01-23 19:19:38 -0500

    Adds changes

--- app/models/user_activity.rb
+++ app/models/user_activity.rb
@@ -1,4 +1,12 @@
 class UserActivity < ApplicationRecord
   belongs_to :user
   belongs_to :activity
+
+  enum rating: {
+    "Me senti mal" => 0,
+    "Me senti normal" => 1,
+    "Me senti bien" => 2,
+    "Me senti Excelente" => 3
+  }
+  
 end
