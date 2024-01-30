02a3d9085c55d8908a7bfc94703f695ecf46acac
Juan Felipe <juanfrg2001@gmail.com>
2024-01-30 17:48:09 -0500

    Modifies push bugs

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,11 +2,20 @@ class NotificationsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
   def create 
-    @notification = WebPushNotification.new(user_id: current_user.id, endpoint: params[:endpoint], auth_key: params[:keys][:auth], p256dh_key: params[:keys][:p256dh] )
-    if @notification.save 
-      render json: @notification
-    else 
-      render json: @notification.errors.full_messages
+    if !WebPushNotification.where(user_id: current_user.id).present?
+      @notification = WebPushNotification.new(user_id: current_user.id, endpoint: params[:endpoint], auth_key: params[:keys][:auth], p256dh_key: params[:keys][:p256dh] )
+      if @notification.save 
+        render json: @notification
+      else 
+        render json: @notification.errors.full_messages
+      end
+    else
+      @notification =  WebPushNotification.where(user_id: current_user.id).last
+      if @notification.update(endpoint: params[:endpoint], auth_key: params[:keys][:auth], p256dh_key: params[:keys][:p256dh] )
+        render json: @notification
+      else
+        render json: @notification.errors.full_messages
+      end
     end
   end
 end
