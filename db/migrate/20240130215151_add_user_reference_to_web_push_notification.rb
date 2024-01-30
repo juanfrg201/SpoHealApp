4bdef99e22f3a5dcc1df952187c6af51be93b68a
Juan Felipe <juanfrg2001@gmail.com>
2024-01-30 17:14:24 -0500

    Adds changes

--- db/migrate/20240130215151_add_user_reference_to_web_push_notification.rb
+++
@@ -1,5 +0,0 @@
-class AddUserReferenceToWebPushNotification < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :web_push_notifications, :user, null: false, foreign_key: true
-  end
-end
