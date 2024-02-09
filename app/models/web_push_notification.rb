f647ccf900c560ccc556f025f3967370719a6e75
Juan Felipe <juanfrg2001@gmail.com>
2024-02-08 20:05:27 -0500

    Adds corrections

--- app/models/web_push_notification.rb
+++ app/models/web_push_notification.rb
@@ -2,6 +2,6 @@ class WebPushNotification < ApplicationRecord
   belongs_to :user
 
   def send_message(message)
-    WebPush.payload_send(message: message, endpoint: endpoint, p256dh: p256dh_key, auth: auth_key, vapid: { private_key: "1RVcXnTjZzPFycE8gS04aL1rOuF8AlMJZjcuqzLOdkm=", public_key: "BEwam3uGmhVbiFaA5XhbcDL7ykh85fq2e7IvNAgy_ZvfnpUukPAzV_DnyZlEb2q017tR9zoKZTzpybOgARgoBgU=" } )
+    WebPush.payload_send(message: message, endpoint: endpoint, p256dh: p256dh_key, auth: auth_key, vapid: { private_key: "2t5qtUqrbrVWwusdafS8P8TyFXa4Z6lB-ypsSI1k13E=", public_key: "BJG7SDVTBamvuvqQkrtYBhjuf8gY8Ckki64fT86GGd3JD_Sxmv3v2lebdwen7sOw7uBHswXnkfeCuO3_ekA7D80=" } )
   end
 end
