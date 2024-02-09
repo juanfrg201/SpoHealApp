8271f7fe41b0cb7ceeaefe8ce244d981343c1170
Juan Felipe <juanfrg2001@gmail.com>
2024-02-08 19:44:38 -0500

    Adds vapid keys

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -3,7 +3,5 @@ class HomePagesController < ApplicationController
 
   def index
     @active_days = active_user_day_weeks.pluck(:day)
-    vapid_key = WebPush.generate_key
-    WebPushNotification.create(user_id: User.first.id, auth_key: vapid_key.private_key  , p256dh_key: vapid_key.public_key )
   end
 end
--- app/models/web_push_notification.rb
+++ app/models/web_push_notification.rb
@@ -2,6 +2,6 @@ class WebPushNotification < ApplicationRecord
   belongs_to :user
 
   def send_message(message)
-    WebPush.payload_send(message: message, endpoint: endpoint, p256dh: p256dh_key, auth: auth_key, vapid: { private_key: "xJaeHX3_0isyOows1SixOoleif8CURXNBuCkbu2pDAM=", public_key: "BDea7ebh7izOOWNuZWdNyL5dZoodZo_OeIULi4rxs4wku8sqvhXYqUs027vJE9hjF-iwSIRbYuINW4btM-XZgoc=" } )
+    WebPush.payload_send(message: message, endpoint: endpoint, p256dh: p256dh_key, auth: auth_key, vapid: { private_key: "1RVcXnTjZzPFycE8gS04aL1rOuF8AlMJZjcuqzLOdkm=", public_key: "BEwam3uGmhVbiFaA5XhbcDL7ykh85fq2e7IvNAgy_ZvfnpUukPAzV_DnyZlEb2q017tR9zoKZTzpybOgARgoBgU=" } )
   end
 end
