b20fea4bd94b239ce567c20bdf9e73fb8894be0f
Juan Felipe <juanfrg2001@gmail.com>
2024-02-08 20:19:20 -0500

    Modifies

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -9,6 +9,15 @@ class NotificationsController < ApplicationController
       else 
         render json: @notification.errors.full_messages
       end
+    else
+      @notification =  WebPushNotification.where(user_id: current_user.id).last
+      @notification.destroy
+      @notification = WebPushNotification.new(user_id: current_user.id, endpoint: params[:endpoint], auth_key: params[:keys][:auth], p256dh_key: params[:keys][:p256dh] )
+      if @notification.save 
+        render json: @notification
+      else
+        render json: @notification.errors.full_messages
+      end
     end
   end
 end
