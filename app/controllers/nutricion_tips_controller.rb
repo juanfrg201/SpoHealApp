095992109d09c945a869649fd192f9abef130233
Juan Felipe <juanfrg2001@gmail.com>
2024-02-18 18:48:30 -0500

    Adds nutrcion csv uploader

--- app/controllers/nutricion_tips_controller.rb
+++ app/controllers/nutricion_tips_controller.rb
@@ -4,10 +4,20 @@ class NutricionTipsController < ApplicationController
   before_action :set_nutricion_tip, only: [:show]
 
   def index 
-    
+    @nutricion_tips = NutricionTip.paginate(page: params[:page], per_page: 10)
   end
 
   def create
+    if create_params[:csv_file].present?
+      nutricion_tips = Services::NutricionTipsUpload.new(params[:csv_file])
+      if nutricion_tips.perform
+        flash[:notice] = "Se subio correctamente el csv"
+        redirect_to nutricion_tips_path
+      else
+        flash[:error] = "No se pudo subir el archivo"
+        redirect_to nutricion_tips_path
+      end
+    end
   end
 
   def show
---
+++ app/models/services/nutricion_tips_upload.rb
@@ -0,0 +1,30 @@
+require 'csv'
+module Services
+  class NutricionTipsUpload
+    attr_reader :file
+
+    def initialize(file)
+      @file = file
+    end
+
+    def perform
+      if @file.present?
+        call
+      end
+    end
+
+    private
+
+    def call
+      CSV.foreach(file.path, headers: true, encoding: 'ISO-8859-1') do |row|
+        activity_type_params = {
+          name: row['Nombre'],
+          benefits: row['Beneficio'],
+          links: row['Links'].present? ? row['Links'].split("*") : []
+        }
+        NutricionTip.create(activity_type_params)
+      end
+    end
+    
+  end
+end
